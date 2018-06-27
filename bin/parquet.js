94358409a491d9bc2403f686002e4fe730bd440b
thomascool <thomas_ku@yahoo.com>
2018-06-27 00:35:14 -0700

    more features
    support output format as below:
    CSV
    TSV
    PSV
    
    And header off

--- bin/parquet.js
+++ bin/parquet.js
@@ -3,12 +3,14 @@
 // Copyright 2017 Luca-SAS, licensed under the Apache License 2.0
 
 const parquet = require('../index.js');
+const _ = require('underscore');
 
 const argv = require('minimist')(process.argv.slice(2), {
   string: '_',
   boolean: [
     'h', 'help',
-    'V', 'version'
+    'V', 'version',
+    'o', 'headeroff'
   ],
   default: {}
 });
@@ -18,13 +20,15 @@ const help = 'Usage: parquet [options] <command> [<args>]\n' +
   'Command line tool to manipulate parquet files\n' +
   '\n' +
   'Commands:\n' +
-  '  cat file       Print file content on standard output\n' +
-  '  head file      Print the first lines of file\n' +
-  '  info file      Print file metadata\n' +
+  '  cat file               Print file content on standard output\n' +
+  '  head file              Print the first lines of file\n' +
+  '  info file              Print file metadata\n' +
+  '  tsv,csv,psv file       Print file content with delimited output\n' +
   '\n' +
   'Options:\n' +
-  '  -h,--help      Print this help text\n' +
-  '  -V,--version   Print version and exits';
+  '  -h,--help              Print this help text\n' +
+  '  -V,--version           Print version and exits\n' +
+  '  -o,--headeroff         Print file content without header\n';
 
 if (argv.h || argv.help) {
   process.stdout.write(help);
@@ -39,6 +43,15 @@ if (argv.V || argv.version) {
 switch (argv._[0]) {
   case 'cat':
     cat(argv._[1]);
+    break;
+  case 'tsv':
+    xsv(argv._[1], '\t');
+    break;
+  case 'psv':
+    xsv(argv._[1], '|');
+    break;
+  case 'csv':
+    xsv(argv._[1], ',');
     break;
   case 'head':
     cat(argv._[1], 5);
@@ -57,6 +70,19 @@ function cat(file, max) {
   for (var i = 0; i < n; i++) {
     data = reader.rows(1);
     process.stdout.write(JSON.stringify(data[0]) + '\n');
+  }
+}
+
+function xsv(file, delimitor, max) {
+  const reader = new parquet.ParquetReader(file);
+  const info = reader.info();
+  const n = max < info.rows ? max : info.rows;
+  if (!(argv.o || argv.headeroff)) {
+    process.stdout.write(JSON.stringify(_.keys(reader.info().schema)).replace(/^\[|\]/g,'').replace(/,/g,delimitor) + '\n');
+  }
+  for (var i = 0; i < n; i++) {
+    data = reader.rows(1);
+    process.stdout.write(JSON.stringify(data[0]).replace(/^\[|\]/g,'').replace(/,/g,delimitor) + '\n');
   }
 }
 
